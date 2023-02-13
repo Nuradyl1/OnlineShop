77771ed2edd95f3fa121348fadf1d50bdad898dd
Your Name <you@example.com>
2023-02-13 21:38:48 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="w-4/5 text-center bg-slate-500 h-screen">
+      <h1 className='text-red-500'>Hello Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
